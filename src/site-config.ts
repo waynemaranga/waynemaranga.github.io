6487f1b10153bc5b493a0f8569ed1da3163e64ae
WM <wmrngcd@gmail.com>
2024-08-10 16:47:57 +0300

    vitesse-push

---
+++ src/site-config.ts
@@ -0,0 +1,102 @@
+export const siteConfig = {
+  author: 'Wayne Maranga',
+  title: 'Wayne Maranga',
+  subtitle: '🏗️ Civil Engineering | ☁️ Cloud Computing | 📊 Analytics',
+  description: '🏗️ Civil Engineering | ☁️ Cloud Computing | 📊 Analytics',
+  image: {
+    src: '/hero.jpg',
+    alt: 'Website Main Image',
+  },
+  email: 'waynemaranga@gmail.com',
+  socialLinks: [
+    {
+      text: 'GitHub',
+      href: 'https://www.github.com/waynemaranga',
+      icon: 'i-simple-icons-github',
+      header: 'i-ri-github-line',
+    },
+    // {
+    //   text: 'Twitter',
+    //   href: '',
+    //   icon: 'i-simple-icons-x',
+    //   header: 'i-ri-twitter-x-line',
+    // },
+    {
+      text: 'Linkedin',
+      href: 'https://www.linkedin.com/in/wayne-maranga',
+      icon: 'i-simple-icons-linkedin',
+    },
+    // {
+    //   text: 'Instagram',
+    //   href: '',
+    //   icon: 'i-simple-icons-instagram',
+    // },
+    // {
+    //   text: 'Youtube',
+    //   href: '',
+    //   icon: 'i-simple-icons-youtube',
+    // },
+  ],
+  header: {
+    logo: {
+      src: '/favicon.svg',
+      alt: 'Logo Image',
+    },
+    navLinks: [
+      {
+        text: 'Blog',
+        href: '/blog',
+      },
+      {
+        text: 'Notes',
+        href: '/blog/notes',
+      },
+      {
+        text: 'Talks',
+        href: '/blog/talks',
+      },
+      {
+        text: 'Projects',
+        href: '/projects',
+      },
+    ],
+  },
+  page: {
+    blogLinks: [
+      {
+        text: 'Blog',
+        href: '/blog',
+      },
+      {
+        text: 'Notes',
+        href: '/blog/notes',
+      },
+      {
+        text: 'Talks',
+        href: '/blog/talks',
+      },
+    ],
+  },
+  footer: {
+    navLinks: [
+      {
+        text: 'Posts Props',
+        href: '/posts-props',
+      },
+      {
+        text: 'Markdown Style',
+        href: '/md-style',
+      },
+      // {
+      //   text: 'View on Astro',
+      //   href: 'https://astro.build/themes/details/vitesse-theme-for-astro/',
+      // },
+      {
+        text: 'GitHub',
+        href: 'https://github.com/waynemaranga/',
+      },
+    ],
+  },
+}
+
+export default siteConfig
