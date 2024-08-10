921e3626bae92d4955618dbdccf9c120d55a41d1
WM <wmrngcd@gmail.com>
2024-08-10 21:59:18 +0300

    removed rss and github icon (along with all social links) from header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -96,12 +96,13 @@ function toggleNavDrawer() {
       </div>
     </div>
     <div class="flex gap-x-6">
+      <!-- 
       <a
         v-for="link in socialLinks" :key="link.text" :aria-label="`${link.text}`" :class="link.icon" nav-link
         :target="getLinkTarget(link.href)" :href="link.href"
-      />
+      />  -->
 
-      <a nav-link target="_blank" href="/rss.xml" i-ri-rss-line aria-label="RSS" />
+      <!-- <a nav-link target="_blank" href="/rss.xml" i-ri-rss-line aria-label="RSS" /> -->
       <ThemeToggle />
     </div>
   </header>
